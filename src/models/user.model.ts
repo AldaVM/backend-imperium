20690de6adfe7dffc4d6edcfaf365186a46bfdb9
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2020-07-31 14:32:36 -0500

    added validation user put mongoose schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,60 +1,60 @@
-import {Schema,Document,model} from 'mongoose';
-import {compareSync,hashSync,genSaltSync} from 'bcrypt';
+import { Schema, Document, model } from 'mongoose';
+import { compareSync, hashSync, genSaltSync } from 'bcrypt';
 
 
-export interface IUser extends Document{
-    name:string;
-    email:string;
-    password:string;
-    createdAt:number;
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: number;
 }
 
-let valuesVaild:{values:String[],message:String}={
-    values:['SUPER','VIEWER'],
-    message:'{VALUE} not a permitted role'
+let valuesVaild: { values: String[], message: String } = {
+    values: ['SUPER', 'VIEWER'],
+    message: '{VALUE} not a permitted role'
 }
 
-const userSchema=new Schema({
-    name:{
-        type:String,
-        required:[true,'Name is required']
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
     },
-    email:{
-        type:String,
-        required:[true,'Email is required'],
-        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
     },
-    password:{
-        type:String,
-        required:[true,'Password is required']
+    password: {
+        type: String,
+        required: [true, 'Password is required']
     },
-    createdAt:{
-        type:Number,
-        required:false,
+    createdAt: {
+        type: Number,
+        required: false,
     },
-    role:{
-        type:String,
-        required:false,
-        uppercase:true,
-        enum:valuesVaild,
-        default:'VIEWER'
+    role: {
+        type: String,
+        required: false,
+        uppercase: true,
+        enum: valuesVaild,
+        default: 'VIEWER'
     }
 });
 
 
-userSchema.methods.toJSON=function(){
-    let user=this.toObject();
+userSchema.methods.toJSON = function () {
+    let user = this.toObject();
     delete user.password;
     return user;
 }
 
 //:(
-userSchema.methods.comparePasswords =function(password:string){
-    return compareSync(password,this.password);
+userSchema.methods.comparePasswords = function (password: string) {
+    return compareSync(password, this.password);
 }
 
-userSchema.pre("save", async function(next) {
-    let user:any = this;
+userSchema.pre("save", async function (next) {
+    let user: any = this;
     if (!user.isModified('password')) {
         return next();
     }
@@ -64,5 +64,20 @@ userSchema.pre("save", async function(next) {
     next();
 });
 
+userSchema.pre("findOneAndUpdate", async function (next) {
+    const password = await this.getUpdate().password;
+    if (!password) {
+        return next();
+    }
+    try {
+        const salt = genSaltSync(10);
+        const hash = hashSync(password, salt);
+        this.getUpdate().password = hash;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
 
-export default model<IUser>('user',userSchema);
+
+export default model<IUser>('user', userSchema);
