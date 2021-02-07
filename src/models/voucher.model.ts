f2f0f9edb3e7fd6c2b9ca4a26b2676b09c879780
AldaVM <aldairvm95@gmail.com>
2021-02-06 22:30:13 -0500

    obtener timetable por customer y se agrego dependencias para trabajar con archivos pdf

--- src/models/voucher.model.ts
+++ src/models/voucher.model.ts
@@ -18,7 +18,7 @@ const voucherSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "customer",
     autopopulate: {
-      select: ["dni", "names", "surnames"],
+      select: ["dni", "names", "surnames", "timetable"],
       maxDepth: 1,
     },
     required: [true, "customer is required"],
